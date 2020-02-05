d1e12a19b7c04489247987fcb3488db4b0f02818
JParrales <josedparrales@gmail.com>
2020-02-05 16:08:03 -0500

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-char Hello_puts = "\"Programming is like building a multilingual puzzle";
+char *Hello_puts = "\"Programming is like building a multilingual puzzle";
 
 puts(Hello_puts);
 return (0);
