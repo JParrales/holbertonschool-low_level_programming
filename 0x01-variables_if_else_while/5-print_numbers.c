92154ce0b915972f4fdac4111d144a8e2402de61
JParrales <josedparrales@gmail.com>
2020-02-06 22:00:16 -0500

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always will be 0
+**/
+
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+putchar(a);
+
+putchar('\n');
+return (0);
+}
