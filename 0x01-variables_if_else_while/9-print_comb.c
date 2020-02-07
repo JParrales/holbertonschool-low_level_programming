f607f859225089ec65c7c386993a6954cd3b288d
JParrales <josedparrales@gmail.com>
2020-02-06 23:05:53 -0500

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always will be 0
+**/
+
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
