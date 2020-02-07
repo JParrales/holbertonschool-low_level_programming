c22a28a5c3900ba00e35ead34bdeed658d0509e4
JParrales <josedparrales@gmail.com>
2020-02-06 22:48:26 -0500

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always will be 0
+**/
+
+int main(void)
+{
+int a;
+
+ for (a = 48; a <= 57; a++)
+putchar(a);
+
+for (a = 97; a <= 102; a++)
+putchar(a);
+
+putchar('\n');
+
+return (0);
+}
