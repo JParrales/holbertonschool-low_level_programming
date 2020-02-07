854e8926db336267b06dbd51782098f56af8c91e
JParrales <josedparrales@gmail.com>
2020-02-06 21:04:36 -0500

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always will be 0
+**/
+
+int main(void)
+{
+int a;
+
+for (a = 97; a <= 122; a++)
+putchar(a);
+
+return (0);
+}
