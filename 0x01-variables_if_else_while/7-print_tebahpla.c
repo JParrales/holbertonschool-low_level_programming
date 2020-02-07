46f5c882b460b2a80a43e3431b093f7b19595868
JParrales <josedparrales@gmail.com>
2020-02-06 22:32:45 -0500

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always will be 0
+**/
+
+int main(void)
+{
+int a;
+
+for (a = 122; a <= 97; a--)
+putchar(a);
+
+putchar('\n');
+return (0);
+}
