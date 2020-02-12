f616414f29b97f8a49cd28d72f9a9272d24d0ce9
JParrales <josedparrales@gmail.com>
2020-02-11 23:09:48 -0500

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *@void
+ *
+ * Return:alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i <= 27; i++)
+	{
+		char az[27] = "abcdefghijklmnopqrstuvwxyz\n";
+		_putchar(az[i]);
+	}
+}
