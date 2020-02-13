4282de9c343fd546925e57da19edf4d1a912d898
JParrales <josedparrales@gmail.com>
2020-02-13 17:37:09 -0500

    commit Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14 ten times.
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void);
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 20; j++)
+		{
+			char numbers[21] = "01234567891011121314\n";
+
+			_putchar(numbers[j]);
+		}
+	}
+}
+
