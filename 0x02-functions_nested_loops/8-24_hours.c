707da3820be9ec0238f56a2e4cc4d43d1e1392b7
JParrales <josedparrales@gmail.com>
2020-02-14 18:59:19 -0500

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,27 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, s;
 
 	for (i = '0'; i <= '2'; i++)
-		for (j = '0'; j <= '3'; j++)
+		for (j = '0'; j <= '9'; j++)
 			for (k = '0'; k <= '5'; k++)
 				for (l = '0'; l <= '9'; l++)
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					_putchar('\n');
+					s++;
+					if (s <= 1440)
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
+					else
+					{
+						break;
+					}
 				}
 }
 
