673d95ad2aa50905ee785aaf600420d42582d277
JParrales <josedparrales@gmail.com>
2020-03-06 09:34:38 -0500

    '0'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int len1, len2, i, j;
 
+	if (s1 == NULL)
+	{
+		s1 = malloc(1);
+		*s1 = 0;
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = malloc(1);
+		*s2 = 0;
+	}
+
+
 	len1 = _length(s1);
 	len2 = _length(s2);
 
