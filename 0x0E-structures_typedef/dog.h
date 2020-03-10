9843d4055d1445b6af1e6980db90e76ae3c1fe5c
JParrales <josedparrales@gmail.com>
2020-03-09 19:50:15 -0500

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Short description
+* @name: Name.
+* @age: Age.
+* @owner: Owner.
+*
+* Description: Longer description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
