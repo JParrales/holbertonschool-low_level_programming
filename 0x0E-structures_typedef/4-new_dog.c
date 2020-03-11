d683cf7764ad8d6d1f7862d533c851d5e0a8b296
JParrales <josedparrales@gmail.com>
2020-03-10 23:49:03 -0500

    store copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,16 @@
 #include "dog.h"
 
+char *_strcpy(char *dest, char *src);
+
+/**
+ * ney_dog: creates a new dog.
+ * @name: name.
+ * @age: age.
+ * @owner: owner.
+ *
+ * Return: New data of structure.
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mydog;
@@ -13,7 +24,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*mydog).age = age;
 	(*mydog).owner = owner;
 
+	name = malloc(sizeof(name) + 1);
+	owner = malloc(sizeof(owner) + 1);
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	_strcpy(name, (*mydog).name);
+	_strcpy(owner, (*mydog).owner);
+
 	return (mydog);
 
+}
+
+
+/**
+ * _strcpy - copies the string
+ * @dest: Destiny
+ * @src: source
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
