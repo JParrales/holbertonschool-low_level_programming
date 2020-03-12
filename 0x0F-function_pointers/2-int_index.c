9e08df0f5856ba3bee30dcd1d61eb7e8be81113a
JParrales <josedparrales@gmail.com>
2020-03-11 20:41:21 -0500

    '0' X 0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0, array == NULL || cmp == NULL)
 		return (-1);
-
-	if ( array == NULL || cmp == NULL)
-		return (NULL);
 
 	for (i = 0; i < size; ++i)
 	{
-		if (cmp(array[i]) != '0')
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 
