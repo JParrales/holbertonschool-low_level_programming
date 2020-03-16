8ed6765883b42538c8777b5d5a353aecb40aa509
JParrales <josedparrales@gmail.com>
2020-03-15 23:01:03 -0500

    Betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,59 +11,40 @@ void print_all(const char * const format, ...)
 {
 	int i = 0, j = 0, ing = 0;
 	char *varg;
-
 	va_list PrintList;
 
-	while(format[i] != '\0')
+	while (format[i] != '\0')
 	{
 		i++;
 	}
-
 	va_start(PrintList, format);
-
-	while(j <= i)
+	while (j <= i)
 	{
-
 		switch (format[j])
 		{
 			case 'c':
 				printf("%c", va_arg(PrintList, int));
 				break;
-
 			case 'i':
 				printf("%i", va_arg(PrintList, int));
 				break;
-
 			case 'f':
 				printf("%f", va_arg(PrintList, double));
 				break;
-
 			case 's':
-
 				varg = va_arg(PrintList, char *);
-
-				if (varg == NULL)
-					printf("(nil)");
-
-				printf("%s", varg);
+				printf("%s", varg  != NULL ? varg : "(nil)");
 				break;
-
 			default:
 				ing = 1;
 				break;
 		}
-
 		j++;
-
 		if (j != i && ing == 0)
 			printf(", ");
-
 		ing = 0;
-
 	}
-
 	printf("\n");
-
 	va_end(PrintList);
 }
 
