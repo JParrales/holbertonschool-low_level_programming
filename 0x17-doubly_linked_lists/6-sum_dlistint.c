cc05bd8bd55dbe6a1600f0c88f93d60316cf2237
JParrales <josedparrales@gmail.com>
2020-11-30 07:54:40 -0500

    TASK 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,41 +1,17 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: head
- * @idx: index
- * @n: new data
- * Return: the address of the new node, or NULL if it failed
+ * sum_dlistint - sum elements
+ * @head: head
+ * Return: sum elements of doubly linked list
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *new, *actual;
-	unsigned int i;
+	int result;
 
-	if (!h)
-		return (NULL);
+	for (result = 0; head != NULL; head = (*head).next)
+		result += (*head).n;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	actual = *h;
-	for (i = 0; actual && i < idx; i++)
-		actual = (*actual).next;
-
-	if (!actual && i == idx)
-		return (add_dnodeint_end(h, n));
-	else if (actual)
-	{
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-		(*new).n = n;
-		(*actual).prev->next = new;
-		(*new).prev = (*actual).prev;
-		(*actual).prev = new;
-		(*new).next = actual;
-		return (new);
-	}
-	return (NULL);
+	return (result);
 }
